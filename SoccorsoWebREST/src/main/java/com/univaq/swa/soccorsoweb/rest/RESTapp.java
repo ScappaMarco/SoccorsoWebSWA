4811d9a1b697a776f74867f0f1db1ef2aff395d0
ScappaMarco <mscappa0@gmail.com>
2025-07-16 19:01:43 +0200

    restapp.java mod

--- SoccorsoWebREST/src/main/java/com/univaq/swa/soccorsoweb/rest/RESTapp.java
+++ SoccorsoWebREST/src/main/java/com/univaq/swa/soccorsoweb/rest/RESTapp.java
@@ -4,6 +4,23 @@ import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.Feature;
 import jakarta.ws.rs.core.FeatureContext;
 
-public class RESTapp extends Application {
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
+public class RESTapp extends Application {
+    private final Set<Class<?>> classes;
+
+    public RESTapp() {
+        HashSet<Class<?>> c = new HashSet<>();
+
+        //resoyrces, filters, ecc
+
+        classes = Collections.unmodifiableSet(c);
+        }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return this.classes;
+    }
 }
